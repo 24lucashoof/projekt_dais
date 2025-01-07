57f4ef844c273c1f7c2e0291ca82778e7951b851
Ondřej Fojtík <49522182+ondrafojtik@users.noreply.github.com>
2025-01-07 18:25:37 +0100

    Add files via upload

---
+++ DBConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+
+namespace DataLayer
+{
+    public class DBConnector
+    {
+        public static SqlConnectionStringBuilder GetBuilder()
+        {
+            Debug.Assert(false, "connect to DB");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"";
+            builder.UserID = "";
+            builder.Password = "";
+            builder.InitialCatalog = "";
+            
+            return builder;
+        }
+    }
+}
